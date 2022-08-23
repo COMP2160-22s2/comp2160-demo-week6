a36196a548eb5b970dcc359df3b20b7d8aa7587a
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2022-08-23 17:32:48 +1000

    Implemented basic Hopping script

--- COMP2160 Week 6 - Frogger/Assets/Scripts/Hop.cs
+++ COMP2160 Week 6 - Frogger/Assets/Scripts/Hop.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class Hop : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public AnimationCurve hopCurve;
+    public float hopDuration = 0.5; // seconds
+    public float hopDistance = 1;
+    private float hopTimer;
+    private bool isHopping  = false;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
+    {        
+        if (isHopping) {
+            DoHop();
+        }
+        else 
+        {
+            if (Input.GetButtonDown(InputAxes.Up))
+            {
+                StartHop(Vector3.up);
+            }
+            else if (Input.GetButtonDown(InputAxes.Down))
+            {
+                StartHop(Vector3.down);
+            }
+            else if (Input.GetButtonDown(InputAxes.Left))
+            {
+                StartHop(Vector3.left);
+            }
+            else if (Input.GetButtonDown(InputAxes.Right))
+            {
+                StartHop(Vector3.right);
+            }
+        }
+    }
+
+    private void StartHop(Vector3 dir)
     {
-        
+        isHopping = true;
+        startPos = transform.localPosition;
+        endPos = transform.localPosition + dir * hopDistance;
+        hopTimer = 0;
+    }
+
+    private void DoHop()
+    {
+        hopTimer += Time.deltaTime;
+        if (hopTimer > hopDuration)
+        {
+            EndHop();
+        }
+        else {
+            float t = hopTimer / hopDuration;
+            t = hopCurve.Eval(t);
+
+            transform.localPosition = Vector3.Lerp(startPos, endPos);
+        }
+
+    }
+
+    private void EndHop()
+    {
+        transfrom.localPosition = endPos;
+        isHopping = false;
     }
 }
---
+++ COMP2160 Week 6 - Frogger/Assets/Scripts/InputAxes.cs
@@ -0,0 +1,7 @@
+public class InputAxes 
+{
+    public const string Up = "Up";
+    public const string Down = "Down";
+    public const string Left = "Left";
+    public const string Right = "Right";
+}
